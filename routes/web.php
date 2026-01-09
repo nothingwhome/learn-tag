9edbe970c856bb5061a64934133e4573e4a5598d
alan <zuenich55@gmail.com>
2026-01-10 00:58:29 +0700

    add route quiz

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-// Route::get('/home', function () {
-//     return view('home.index');
-// });
+Route::get('/quiz', function () {
+    return view('quiz.quiz');
+});
