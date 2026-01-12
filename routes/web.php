6004f6bbc46e61710a14712d3e007999a338bbff
vinnzy99 <vinnzy99@gmail.com>
2026-01-12 21:53:19 +0700

    memperbaiki navbar

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('component.footer');
+});
+
+Route::get('/quiz', function () {
     return view('quiz.quiz');
 });
 
